3c40942c36f50517dc4598144be14b15851fdea9
Moumen Hallak <moumen95@gmail.com>
2021-06-20 20:56:18 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(arr){
+  const winner = arr.find((e) => e.result == 'W');
+	  if(winner){
+      return winner.year;
+    }
+    else {
+      return undefined;
+    }  
+  
+}
